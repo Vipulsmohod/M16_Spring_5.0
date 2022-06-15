a5e60667d317aa58fe92f36c194f7398c2abbeab
shree <shree@192.168.1.8>
2022-06-15 15:26:09 +0530

    First Commit

---
+++ SpringDepandencyInjection/src/main/java/com/cg/di/Exam.java
@@ -0,0 +1,16 @@
+package com.cg.di;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam {
+
+	public static void main(String[] args) {
+		ApplicationContext c=new ClassPathXmlApplicationContext("beans.xml");
+		Student s1=c.getBean("s1",Student.class);
+		s1.print();
+		/*Student s1=c.getBean("s1",Student.class);
+		s1.print();*/
+
+	}
+
+}
